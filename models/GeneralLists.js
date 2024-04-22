479f0b8d6d2d777476928c90f2657acbca28a8fa
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-22 20:41:31 +0300

    Add general lists schema (#6)

---
+++ models/GeneralLists.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+const TodayListSchema = new Schema({
+  name: { type: String, default: "Today" },
+  tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }]
+});
+
+const PlannedListSchema = new Schema({
+  name: { type: String, default: "Planned" },
+  tasks: [{ type: Schema.Types.ObjectId, ref: "Task", default: []  }]
+});
+
+const AllTasksListSchema = new Schema({
+  name: { type: String, default: "Tasks" },
+  tasks: [{ type: Schema.Types.ObjectId, ref: "Task", default: [] }],
+});
+
+const GeneralListsSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User", },
+  todayList: { type: TodayListSchema },
+  plannedList: { type: PlannedListSchema },
+  allTasksList: { type: AllTasksListSchema },
+});
+
+module.exports = model("GeneralLists", GeneralListsSchema);
