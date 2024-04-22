b7f7f3079738176ad617e9212c2f442a6dbb1dd6
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-22 23:20:15 +0300

    Update task schema (#6)

--- models/Task.js
+++ models/Task.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose')
 const TaskSchema = new Schema({
     listId: [{ type: Schema.Types.ObjectId, ref: "List" }],
     title: { type: String, unique: true, required: true },
+    status: { type: String, default: "active" },
     priority: { type: String, default: "low" },
     description: { type: String, default: "" },
     plannedDate: { type: Date, default: Date.now },
