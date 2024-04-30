03d63dad3b2523dde1e8257390e3c28e2313c418
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-30 15:10:56 +0300

    Add notification controller (#7)

--- controller/notification-controller.js
+++ controller/notification-controller.js
@@ -1,11 +1,58 @@
 const NotificationService = require("../service/notification-service");
 
-class NotificationController {
-  async createNotification(req, res) {
-    const { userId, text } = req.body;
-    const notification = await NotificationService.createNotification(userId, text);
-    return res.json(notification);
+class notificationController {
+  async createNotification(req, res, next) {
+    try {
+      const notification = await NotificationService.createNotification(
+        req.body
+      );
+      return res.json(notification);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async getNotification(req, res, next) {
+    try {
+      const notifications = await NotificationService.getNotification(
+        req.user.id
+      );
+      return res.json(notifications);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async deleteNotification(req, res, next) {
+    try {
+      const notification = await NotificationService.deleteNotification(
+        req.body
+      );
+      return res.json(notification);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async getAllNotifications(req, res, next) {
+    try {
+      const notifications = await NotificationService.getAllNotifications();
+      return res.json(notifications);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async updateNotification(req, res, next) {
+    try {
+      const notification = await NotificationService.updateNotification(
+        req.body
+      );
+      return res.json(notification);
+    } catch (err) {
+      next(err);
+    }
   }
 }
 
-module.exports = new NotificationController();
+module.exports = new notificationController();
