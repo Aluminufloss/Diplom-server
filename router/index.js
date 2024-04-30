c6325a50a6bb37862e9e9ac1cd616e5109e2f061
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-30 15:11:26 +0300

    Add routes for notification (#7)

--- router/index.js
+++ router/index.js
@@ -18,18 +18,23 @@ router.post("/sendChangeLink", userController.sendChangePasswordLink);
 router.post("/changePassword", userController.changePassword);
 router.get("/activate/:link", userController.activate);
 
-router.post("/create/list", listController.createList);
-router.post("/delete/list", listController.deleteList);
+router.post("/create/list", authMiddleware, listController.createList);
+router.post("/delete/list", authMiddleware, listController.deleteList);
 router.post("/create/generalLists", listController.createGeneralLists);
 router.post("/get/list", listController.getList);
+router.post("/get/lists", authMiddleware, listController.getLists);
+router.post("/get/listName", listController.getListName);
+router.post("/getAll/listsName", listController.getAllListsNames);
+router.post("/getTasksByListId/list", authMiddleware, listController.getTasksByListId);
 
-router.post("/create/task", taskController.createTask);
-router.post("/delete/task", taskController.deleteTask);
-router.post("/update/task", taskController.updateTask);
-router.post("/getToday/task", taskController.getTodayTasks);
-router.post("/getPlanned/task", taskController.getPlannedTasks);
-router.post("/getAll/task", taskController.getAllTasks);
-router.post("/get/task", taskController.getTask);
+router.post("/create/task", authMiddleware, taskController.createTask);
+router.post("/delete/task", authMiddleware, taskController.deleteTask);
+router.post("/update/task", authMiddleware, taskController.updateTask);
+router.post("/getToday/task", authMiddleware, taskController.getTodayTasks);
+router.post("/getPlanned/task", authMiddleware, taskController.getPlannedTasks);
+router.post("/getAll/task", authMiddleware, taskController.getAllTasks);
+router.post("/get/task", authMiddleware, taskController.getTask);
+router.post("/changeStatus/task", authMiddleware, taskController.changeTaskStatus);
 
 router.post("/create/group", groupController.createGroup);
 router.post("/delete/group", groupController.deleteGroup);
@@ -39,7 +44,8 @@ router.post("/removeList/group", groupController.removeListFromGroup);
 
 router.post("/create/notification", notificationController.createNotification);
 router.post("/delete/notification", notificationController.deleteNotification);
-router.post("/get/notification", notificationController.getNotifications);
+router.post("/get/notification", notificationController.getNotification);
 router.get("/getAll/notification", notificationController.getAllNotifications);
+router.post("/update/notification", notificationController.updateNotification);
 
 module.exports = router;
