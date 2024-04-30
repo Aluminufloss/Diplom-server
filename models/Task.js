2fe84acc18b741cae66177dd60d2b68c7c6791db
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-30 15:04:24 +0300

    Update task model (#6)

--- models/Task.js
+++ models/Task.js
@@ -1,14 +1,19 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require('mongoose');
+
+const RepeatDaySchema = new Schema({
+    day: { type: String, required: true },
+    isSelected: { type: Boolean, default: false },
+})
 
 const TaskSchema = new Schema({
     listId: [{ type: Schema.Types.ObjectId, ref: "List" }],
-    title: { type: String, unique: true, required: true },
+    title: { type: String, unique: true, required: true, default: "" },
     status: { type: String, default: "active" },
     priority: { type: String, default: "low" },
     description: { type: String, default: "" },
     plannedDate: { type: Date, default: Date.now },
-    repeat: { type: String },
+    repeatDays: { type: [RepeatDaySchema], default: [] },
     category: { type: String },
-})
+});
 
 module.exports = model('Task', TaskSchema)
