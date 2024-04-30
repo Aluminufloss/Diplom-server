2b676abc3fbef2ad883376a3782a70f71f0d7d42
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-30 15:10:35 +0300

    Update notification service (#7)

--- service/notification-service.js
+++ service/notification-service.js
@@ -1,25 +1,28 @@
-const NotificationModel = require('../models/Notification');
+const NotificationModel = require("../models/Notification");
 
-class NotificationService  {
-  async createNotification(userId, text) {
-    const notification = await NotificationModel.create({ userId, text })
-    return notification;
+class NotificationService {
+  async createNotification(notification) {
+    const newNotification = await NotificationModel.create(notification);
+    return newNotification;
   }
 
-  async getNotification(taskId, userId) {
-    const notification = await NotificationModel.find({ userId, taskId });
-    return notification;
-  }
-
-  async deleteNotification(notificationId) {
-    const notification = await NotificationModel.findByIdAndDelete(notificationId);
+  async getNotification(userId) {
+    const notification = await NotificationModel.findOne({ userId });
     return notification;
   }
 
   async getAllNotifications(userId) {
-    const notifications = await NotificationModel.find(userId);
+    const notifications = await NotificationModel.find({ userId });
     return notifications;
+  }
+
+  async deleteNotification(notificationId) {
+    await NotificationModel.deleteOne({ _id: notificationId });
+  }
+
+  async updateNotification(notificationId, notification) {
+    await NotificationModel.updateOne({ _id: notificationId }, notification);
   }
 }
 
-module.exports = new NotificationService()
+module.exports = new NotificationService();
