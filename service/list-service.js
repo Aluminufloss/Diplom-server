171670350ea090424812cbdee7a5b16d050b9275
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-01 21:04:59 +0300

    Add missing lists methods to list service (#6)

--- service/list-service.js
+++ service/list-service.js
@@ -2,6 +2,11 @@ const ListModel = require("../models/List");
 const UserModel = require("../models/User");
 const GeneralListsModel = require ("../models/GeneralLists");
 
+const ListDto = require("../dtos/list-dto");
+const TaskDto = require("../dtos/task-dto");
+
+const ApiError = require("../exceptions/api-error");
+
 class ListService {
   async createList(name, userId) {
     const candidate = await UserModel.findOne({ _id: userId });
@@ -13,7 +18,7 @@ class ListService {
     }
 
     const list = await ListModel.create({ name, userId });
-    return list;
+    return new ListDto(list);
   }
 
   async createGeneralLists(userId) {
@@ -44,6 +49,33 @@ class ListService {
   async getList(listId) {
     const list = await ListModel.findOne({ _id: listId });
     return list;
+  }
+
+  async getLists(userId) {
+    const lists = await ListModel.find({ userId: userId });
+    return lists.map((list) => new ListDto(list));
+  }
+
+
+  async getListName(listId) {
+    const list = await ListModel.findOne({ _id: listId });
+    return list.name; 
+  }
+
+  async getAllListsNames(listsId) {
+    const listsNames = [];
+
+    for (const listId of listsId) {
+      const listName = await this.getListName(listId);
+      listsNames.push(listName);
+    }
+
+    return listsNames;
+  }
+
+  async getTasksByListId(listId) {
+    const list = await ListModel.findOne({ _id: listId });
+    return list.tasks.map((task) => new TaskDto(task));
   }
 }
 
