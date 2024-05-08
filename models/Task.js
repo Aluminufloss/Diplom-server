7581e10e8d73c0f66726f22d3edd16c2cf7ec817
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-08 21:19:54 +0300

    Update task model (title is no longer unique) (#6)

--- models/Task.js
+++ models/Task.js
@@ -1,19 +1,21 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+
+const { getDateInISOFormat } = require("../utils/datesUtils");
 
 const RepeatDaySchema = new Schema({
-    day: { type: String, required: true },
-    isSelected: { type: Boolean, default: false },
-})
-
-const TaskSchema = new Schema({
-    listId: [{ type: Schema.Types.ObjectId, ref: "List" }],
-    title: { type: String, unique: true, required: true, default: "" },
-    status: { type: String, default: "active" },
-    priority: { type: String, default: "low" },
-    description: { type: String, default: "" },
-    plannedDate: { type: String, default: Date.now.toString() },
-    repeatDays: { type: [RepeatDaySchema], default: [] },
-    category: { type: String },
+  day: { type: String, required: true },
+  isSelected: { type: Boolean, default: false },
 });
 
-module.exports = model('Task', TaskSchema);
+const TaskSchema = new Schema({
+  listId: [{ type: Schema.Types.ObjectId, ref: "List" }],
+  title: { type: String, required: true, default: "" },
+  status: { type: String, default: "active" },
+  priority: { type: String, default: "low" },
+  description: { type: String, default: "" },
+  plannedDate: { type: String, default: getDateInISOFormat() },
+  repeatDays: { type: [RepeatDaySchema], default: [] },
+  category: { type: String, default: "" },
+});
+
+module.exports = model("Task", TaskSchema);
