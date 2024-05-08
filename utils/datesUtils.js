71d0a32fe7bddd4cbc4652dd374aa899c6a34f66
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-08 21:20:55 +0300

    Update isFirstDateAfterSecond method and add getDateInIsoFormat function to date utils (#6)

--- utils/datesUtils.js
+++ utils/datesUtils.js
@@ -11,11 +11,17 @@ const isDatesEqual = (date1, date2) => {
 }
 
 const isFirstDateAfterSecond = (date1, date2) => {
-  return isAfter(new Date(date1), new Date(date2));
+  const currentDate = format(date1, "yyyy-MM-dd");
+  const plannedDate = format(date2, "yyyy-MM-dd");
+  return isAfter(currentDate, plannedDate);
 }
 
 const getDayIndex = (date) => {
   return getDay(date);
 }
 
-module.exports = { isDatesEqual, isFirstDateAfterSecond, getDayIndex };
+const getDateInISOFormat = () => {
+  return new Date().toISOString()
+}
+
+module.exports = { isDatesEqual, isFirstDateAfterSecond, getDayIndex, getDateInISOFormat };
