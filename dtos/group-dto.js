8f09e958af52373c335966c44e01823cb81705b9
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-09 20:29:26 +0300

    Update group dto (#6)

--- dtos/group-dto.js
+++ dtos/group-dto.js
@@ -2,12 +2,10 @@ module.exports = class GroupDto {
   id;
 	name;
 	lists;
-	userId;
 
   constructor(group) {
     this.id = group._id;
     this.name = group.name;
     this.lists = group.lists;
-    this.userId = group.userId;
   }
 }
