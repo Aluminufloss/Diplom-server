77402af8e1b521bb7229ce86c58f4b866b007804
Ильченко Назар <nilchenko2020@gmail.com>
2024-10-25 21:33:48 +0300

    Update error-middleware on ts (#2)

---
+++ middleware/error-middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import ApiError from "../exceptions/api-error";
+
+export default function (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response {
+  if (err instanceof ApiError) {
+    return res
+      .status(err.status)
+      .json({ message: err.message, errors: err.errors });
+  }
+
+  return res.status(500).json({ message: "Непредвиденная ошибка" });
+}
