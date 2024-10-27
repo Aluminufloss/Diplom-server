7ef276ad22d3a7c54dfaa7ed8af749faded8a68c
Ильченко Назар <nilchenko2020@gmail.com>
2024-10-27 21:13:48 +0300

    Update IToken service interface types (#6)

--- src/services/token/ITokenService.ts
+++ src/services/token/ITokenService.ts
@@ -1,10 +1,16 @@
 import { JwtPayload } from "jsonwebtoken";
 
+import { IUserDto } from "@/types/IUser";
+import { ITokenDocument } from "@/types/IToken";
+
 export interface ITokenService {
-  generateTokens(payload: object): { accessToken: string; refreshToken: string };
+  generateTokens(payload: IUserDto): {
+    accessToken: string;
+    refreshToken: string;
+  };
   validateAccessToken(token: string): JwtPayload | null;
   validateRefreshToken(token: string): JwtPayload | null;
-  saveToken(userId: string, refreshToken: string): Promise<Document>;
-  removeToken(refreshToken: string): Promise<{ deletedCount?: number }>;
-  findToken(refreshToken: string): Promise<Document | null>;
+  saveToken(userId: string, refreshToken: string): Promise<ITokenDocument>;
+  removeToken(refreshToken: string): Promise<void>;
+  findToken(refreshToken: string): Promise<ITokenDocument | null>;
 }
