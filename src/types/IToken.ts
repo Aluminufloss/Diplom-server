258e06dd88cf4cb87f6ea620652fd7c4c67dc31c
Ильченко Назар <nilchenko2020@gmail.com>
2024-10-27 21:14:07 +0300

    Add new IToken document type (#6)

--- src/types/IToken.ts
+++ src/types/IToken.ts
@@ -3,9 +3,12 @@ import { Types } from "mongoose";
 export interface IToken {
   user: Types.ObjectId;
   refreshToken: string;
+  accessToken: string;
 }
 
 export type TokensType = {
   accessToken: string;
   refreshToken: string;
 };
+
+export interface ITokenDocument extends IToken, Document {}
