267c72a23d867325a2ebb3122ab060a0d5252a86
Ильченко Назар <nilchenko2020@gmail.com>
2024-10-27 20:27:11 +0300

    Add types for API responses for user (#4)

--- src/types/IUser.ts
+++ src/types/IUser.ts
@@ -1,4 +1,5 @@
 export interface IUser {
+  id: string;
   email: string;
   password: string;
   isActivated: boolean;
@@ -6,3 +7,30 @@ export interface IUser {
   activationLink?: string;
   username?: string;
 }
+
+export interface IUserDto {
+  email: string;
+  id: string;
+  username?: string;
+}
+
+export type UserApiRequestType = {
+  email: string;
+  password: string;
+  username?: string;
+  shouldRememberMe?: boolean;
+}
+
+export type UserApiResponseType = {
+  user: IUserDto;
+  accessToken: string;
+  refreshToken: string;
+}
+
+export type UserLoginRequestType = {
+  email: string;
+  password: string;
+  shouldRememberMe?: boolean;
+}
+
+export interface IUserDocument extends IUser, Document {}
