43061d1df83488d658c47da7d9bd8c6de3a9d872
Ильченко Назар <nilchenko2020@gmail.com>
2024-10-27 21:13:29 +0300

    Update token model types (#6)

--- src/models/Token.ts
+++ src/models/Token.ts
@@ -1,9 +1,11 @@
-import { Schema, model } from "mongoose";
-import { IToken } from "../types/IToken";
+import { Model, Schema, model } from "mongoose";
+import { ITokenDocument } from "@/types/IToken";
 
-const TokenSchema = new Schema<IToken>({
+const TokenSchema = new Schema<ITokenDocument>({
   user: { type: Schema.Types.ObjectId, ref: "User" },
   refreshToken: { type: String, required: true },
 });
 
-export default model('Token', TokenSchema);
+const TokenModel: Model<ITokenDocument> = model<ITokenDocument>("User", TokenSchema);
+
+export default TokenModel;
